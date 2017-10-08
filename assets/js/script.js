62ab790888900b79584955544a020a47540719ab
Osman Emara <emaraosman@gmail.com>
2017-10-08 13:02:00 -0400

    add new background on shleepy.html and word list functionality with letter catcher

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
 
   console.log('jq working')
 
-
+  $(".word-list").click(function(){
+    $("#word-list-container").fadeOut();
+    wordArray = $(this).text().split("")
+  })
 
 
 });
