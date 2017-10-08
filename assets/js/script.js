286ad893d32d0313791d9ac85e166c7c3dcf4d24
Osman Emara <emaraosman@gmail.com>
2017-10-08 16:09:01 -0400

    add min height to prevent mobile keyboard bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ $(document).ready(function(){
     wordArray.forEach(function(item){
       $("#game-board #letter-stack").append(
         $(`<div class="letter-box" style="height:calc(100%/${wordArray.length})"></div>`)
-        .append(`<div class="letter">${item}</div>`)
+        .append(`<div class="letter">${item} :</div>`)
         .append(`<input value="${item}"></input>`)
         .append("<div class='submit-word'>+</div>")
         // .text(`${item}`)
