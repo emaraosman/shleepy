5fb6617b48c09e5829641ef6a10c794700958024
Osman Emara <emaraosman@gmail.com>
2017-10-08 15:59:21 -0400

    create css only fix for long words to fit on screen by dividing by wordarray.length

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,21 @@
 $(document).ready(function(){
-
   console.log('jq working')
 
   $(".word-list").click(function(){
     $("#word-list-container").fadeOut();
-    wordArray = $(this).text().split("")
+    $("#game-board").fadeIn();
+    wordArray = $(this).text().toUpperCase().split("");
+
+    wordArray.forEach(function(item){
+      $("#game-board #letter-stack").append(
+        $(`<div class="letter-box" style="height:calc(100%/${wordArray.length})"></div>`)
+        .append(`<div class="letter">${item}</div>`)
+        .append(`<input value="${item}"></input>`)
+        .append("<div class='submit-word'>+</div>")
+        // .text(`${item}`)
+        );
+    });
+
   })
 
 
