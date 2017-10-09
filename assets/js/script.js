116deab7fec2121553e5bbb120be1a1f04730d50
Osman Emara <emaraosman@gmail.com>
2017-10-09 18:52:39 -0400

    create new list button which clears game board and creates list of new submitted words to choose from

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
   console.log('jq working')
   submitArray = [];
-  wordup=""
+  wordUp="";
 
-  $(".word-list").one('click',function(){
+  $("body").on('click','.word-list',function(){
     $("#word-list-container").fadeOut();
     $("#game-board").fadeIn();
     wordArray = $(this).text().toUpperCase().split("");
@@ -12,26 +12,41 @@ $(document).ready(function(){
       $("#game-board #letter-stack").append(
         $(`<div class="letter-box" style="height:calc(99%/${wordArray.length})"></div>`)
         .append(`<div class="letter">${item}</div>`)
-        .append(`<form action="enterToSubmit()"> <input class="word" type="text" name="word" value="${item}" maxlength="9"> </form>`)
+        // .append(`<form action="enterToSubmit()"> <input class="word" type="text" name="word" value="${item}" maxlength="9"> </form>`)
+        .append(` <input class="word" type="text" name="word" value="${item}" maxlength="9">`)
         .append("<div class='submit-word'>+</div>")
         // .text(`${item}`)
+
         );
+
     });
+    $("#game-board").append("<div id='new-list-button'>new list</div>");
+  });
+
+
+ $(document).ready(function(){
+  $("body").on("click",".submit-word", function(){
+    wordUp = $(this).prev().val();
+    submitArray.push(wordUp);
+
+    $(this).prev().val("");
+  });
+});
+
+
+$(document).ready(function(){
+  $("body").on("click","#new-list-button",function(){
+        // $("#game-board").empty(); FIND SOMEWHERE FOR THIS
+    $("#game-board").fadeOut();
+    $("#word-list-container").empty();
+    submitArray.forEach(function(item){
+      $("#word-list-container").append(
+        $(`<p class="word-list word1 animated fadeInRight">${item}</p>`)
+      );
+    })
+    $("#word-list-container").fadeIn()
   })
-
-  //use this function to press enter and submit the value into submit array.
-  function enterToSubmit(){
-    submitArray.push($(this).val())
-    $(this).val()="";
-  }
-
-  //use this function to click on submit button and submit value into submit array.
-  $(".submit-word").one("click",function(){
-    wordup = $("input")[1].val()
-    submitArray.push(wordup);
-    $("input")[1].val()="";
-  })
-
+})
 
 
 
